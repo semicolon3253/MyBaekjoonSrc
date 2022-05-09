f249b537cc8efcc4ed92553c72207ecd9072066a
Semicolon3253 <47348424+semicolon3253@users.noreply.github.com>
2022-05-09 21:29:52 +0900

    [Bronze I] Title: 평균, Time: 80 ms, Memory: 11944 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -11,13 +11,11 @@ public class Main {
         double[] score_list = new double[count];
 
         StringTokenizer stk = new StringTokenizer(br.readLine());
+        double M = 0;
         for(int i=0; i<score_list.length; i++){
             int score = Integer.parseInt(stk.nextToken());
             score_list[i] = score;
-        }
-
-        double M = 0;
-        for(int i=0; i<score_list.length; i++){
+            
             if(score_list[i] >= M){
                 M = score_list[i];
             }
