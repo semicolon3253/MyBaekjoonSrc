55acd23e8656ee0bc74fbd3f9853edfc17c9235b
Semicolon3253 <47348424+semicolon3253@users.noreply.github.com>
2022-05-09 21:26:36 +0900

    [Bronze I] Title: 평균, Time: 80 ms, Memory: 11948 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count= Integer.parseInt(br.readLine());
+        double[] score_list = new double[count];
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for(int i=0; i<score_list.length; i++){
+            int score = Integer.parseInt(stk.nextToken());
+            score_list[i] = score;
+        }
+
+        double M = 0;
+        for(int i=0; i<score_list.length; i++){
+            if(score_list[i] >= M){
+                M = score_list[i];
+            }
+        }
+
+        double sum = 0;
+        for(int i=0; i<score_list.length; i++){
+            sum += score_list[i] / M * 100;
+        }
+        bw.write(String.valueOf(sum / score_list.length));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
